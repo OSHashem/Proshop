4fc69b551e443a2bc7865f6f205bd7f5fb045641
OSHashem <oh90002@gmail.com>
2026-09-22 15:35:50 +0300

    Redux Setup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,39 +1,32 @@
-import {useState,useEffect} from 'react'
-import axios from 'axios'
-
-import {Row, Col} from 'react-bootstrap'
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-// import products from '../products'
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    // Fetch products from the backend API
-    const fetchProducts = async () => {
-      try {
-        const {data} = await axios.get('/api/products');
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-      {products.map((product) => (
-        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-          <Product product={product} />
-        </Col>
-      ))}
-    </Row>
-    </>
-  )
-} 
 
-export default HomeScreen
+      {isLoading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant='danger'>{error?.data?.message}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
